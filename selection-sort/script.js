9714fec7b0a551ea99dc16a899d5d7fb97cef648
arashforghanbin <arashforghanbin@gmail.com>
2026-06-22 14:50:05 +0330

    ADD: selection sort added

---
+++ selection-sort/script.js
@@ -0,0 +1,23 @@
+function selectionSort(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    let minIndex = i;
+
+    // find smallest in remaining array
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    // swap
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+
+  return arr;
+}
+
+console.log(selectionSort([5, 3, 6, 2]));
